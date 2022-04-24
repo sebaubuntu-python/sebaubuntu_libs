d112dd1bbf043296dc4377c9d235247c248a3cf1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-24 23:33:13 +0200

    sebaubuntu_libs: liblogging: Add a common setup_logging()
    Change-Id: If65e7525eac2ee918ac7ba1de33abc34b9a7fe97

--- sebaubuntu_libs/liblogging/__init__.py
+++ sebaubuntu_libs/liblogging/__init__.py
@@ -3,8 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-"""Logging wrapper."""
+"""Logging utils."""
 
+from logging import basicConfig, INFO, DEBUG
 from logging import debug, info, warning, error, fatal
 
 LOGD = debug
@@ -12,3 +13,10 @@ LOGI = info
 LOGW = warning
 LOGE = error
 LOGF = fatal
+
+def setup_logging(verbose: bool = False):
+	if verbose:
+		basicConfig(format='[%(filename)s:%(lineno)s %(levelname)s] %(funcName)s: %(message)s',
+		            level=DEBUG)
+	else:
+		basicConfig(format='[%(levelname)s] %(message)s', level=INFO)
